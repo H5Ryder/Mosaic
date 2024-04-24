272298032429c12181ad4d63fad49bd5994a5648
H5Ryder <ryderharris@icloud.com>
2024-04-24 14:52:24 +0800

    Add Landing & Guide page - fix import error

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import strokes from "../assets/strokes.jpg";
 
 function LandingPage() {
   const navigate = useNavigate();
